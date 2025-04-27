4a4c21200b048e483fb403627412adb4a2daf7b1
2Estella <krsd0501@naver.com>
2025-04-27 18:51:10 +0900

    🎨 code formatting
    - ESLint와 Prettier에 맞춰 코드 포맷팅
    - 코드 스타일 일관성 유지

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import { BrowserRouter as Router } from "react-router-dom"
-import Header from "./widgets/ui/Header.tsx"
-import Footer from "./widgets/ui/Footer.tsx"
-import PostsManagerPage from "./pages/PostsManagerPage.tsx"
+import { BrowserRouter as Router } from 'react-router-dom';
 
-const App = () => {
-  return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow container mx-auto px-4 py-8">
-          <PostsManagerPage />
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  )
-}
+import PostsManagerPage from './pages/PostsManagerPage';
+import Footer from './widgets/ui/Footer';
+import Header from './widgets/ui/Header';
 
-export default App
+const App = () => (
+  <Router>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow container mx-auto px-4 py-8">
+        <PostsManagerPage />
+      </main>
+      <Footer />
+    </div>
+  </Router>
+);
+
+export default App;
